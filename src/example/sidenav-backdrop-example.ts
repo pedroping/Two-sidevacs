d648cfac2762f70e1a3f0d72915d8a7bcc6db37b
pedroping <84201034+pedroping@users.noreply.github.com>
2023-06-16 10:12:39 -0300

    3 files modified, 1 added, 1 deleted

--- src/example/sidenav-backdrop-example.ts
+++ src/example/sidenav-backdrop-example.ts
@@ -1,19 +1,27 @@
-import {Component} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 /** @title Drawer with explicit backdrop setting */
 @Component({
   selector: 'sidenav-backdrop-example',
   templateUrl: 'sidenav-backdrop-example.html',
-  styleUrls: ['sidenav-backdrop-example.css'],
+  styleUrls: ['sidenav-backdrop-example.scss'],
   standalone: true,
-  imports: [MatSidenavModule, MatFormFieldModule, MatSelectModule, MatButtonModule],
+  imports: [
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+  ],
 })
 export class SidenavBackdropExample {}
-
 
 /**  Copyright 2023 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
